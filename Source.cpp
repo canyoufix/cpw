25e4d8b14d755bf0944ce5f46b09752a475642fb
raspberry tea <sekiro.fpl@gmail.com>
2022-10-09 18:26:28 +0300

    v1.3F

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,20 @@
 
 #define BUF_SIZE 256
 
+int changeBuffer(CHAR Buffer[], DWORD &nIn, INT numFromCMD);
+
 int wmain(int argc, wchar_t** argv) {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
 	HANDLE hIn, hOut;
+	HINSTANCE hLib;
+
 	DWORD nIn, nOut;
 	CHAR Buffer[BUF_SIZE];
 	LPCWSTR outFile = L"out.txt";
 
-	if (argc != 3) {
+	if (argc != 3) { //Проверка количества аргументов
 		printf("Использование: cpw test.txt Число\n");
 		return  1;
 	}
@@ -32,38 +36,12 @@ int wmain(int argc, wchar_t** argv) {
 	}
 
 	while (ReadFile(hIn, Buffer, BUF_SIZE, &nIn, NULL) && nIn > 0) {
-		INT numOfNum = 0, index = 0;
-		while (index < nIn) { //Количество цифр во всем буффере
-			if (Buffer[index] >= '0' && Buffer[index] <= '9') {
-				numOfNum++;
-			}
-			index++;
-		}
-
 		INT numFromCMD = _wtoi(argv[2]);
-		if (numOfNum == 0 || numFromCMD <= 0) { //Проверка количества введнных цифр
-			printf("Нельзя заменено 0 цифр(ы), либо в документе они отсутствуют.\n");
-			return 5;
-		}
-		if (numOfNum >= numFromCMD) {
-			printf("Будет заменено %d цифр(ы).\n", numFromCMD);
-		}
-		else {
-			printf("Во всем документе %d цифр(ы), а не %d, поэтому будут заменены все.\n\n", numOfNum, numFromCMD);
-			numFromCMD = numOfNum;
-		}
 
-		index = 0;
-		INT numOfReplace = 0;
-		while (index < nIn) {
-			if (Buffer[index] >= '0' && Buffer[index] <= '9') {
-				Buffer[index] = ' ';
-				numOfReplace++;
-				if (numOfReplace == numFromCMD) {
-					index = nIn;
-				}
-			}
-			index++;
+		INT numOfReplace = changeBuffer(Buffer, nIn, numFromCMD);
+
+		if (numOfReplace == -1) {
+			return 5;
 		}
 
 		WriteFile(hOut, Buffer, nIn, &nOut, NULL);
@@ -71,10 +49,49 @@ int wmain(int argc, wchar_t** argv) {
 			printf("Неустранимая ошибка записи: %x\n", GetLastError());
 			return 4;
 		}
-		printf("Количество замененных цифр: %d", numFromCMD);
+		
+		printf("Количество замененных цифр: %d", numOfReplace);
 	}
 
 	CloseHandle(hIn);
 	CloseHandle(hOut);
 	return 0;
+}
+
+int changeBuffer(CHAR Buffer[], DWORD &nIn, INT numFromCMD) {
+	INT numOfNum = 0, index = 0;
+
+	while (index < nIn) { //Количество цифр во всем буффере
+		if (Buffer[index] >= '0' && Buffer[index] <= '9') {
+			numOfNum++;
+		}
+		index++;
+	}
+
+	if (numOfNum == 0 || numFromCMD <= 0) { //Проверка количества введнных цифр
+		printf("Нельзя заменено 0 цифр(ы), либо в документе они отсутствуют.\n");
+		return -1;
+	}
+	if (numOfNum >= numFromCMD) {
+		printf("Будет заменено %d цифр(ы).\n", numFromCMD);
+	}
+	else {
+		printf("Во всем документе %d цифр(ы), а не %d, поэтому будут заменены все.\n\n", numOfNum, numFromCMD);
+		numFromCMD = numOfNum;
+	}
+
+	index = 0;
+	INT numOfReplace = 0;
+	while (index < nIn) {
+		if (Buffer[index] >= '0' && Buffer[index] <= '9') {
+			Buffer[index] = ' ';
+			numOfReplace++;
+			if (numOfReplace == numFromCMD) {
+				index = nIn;
+			}
+		}
+		index++;
+	}
+
+	return numOfReplace;
 }
