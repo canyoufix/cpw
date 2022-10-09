fe31e212e86967f2aaf817b7ee8cec19505b2587
raspberry tea <sekiro.fpl@gmail.com>
2022-10-09 17:45:17 +0300

    v1.2

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,23 @@
 #include <Windows.h>
 #include <stdio.h> 
+
 #define BUF_SIZE 256
 
 int wmain(int argc, wchar_t** argv) {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+
 	HANDLE hIn, hOut;
 	DWORD nIn, nOut;
 	CHAR Buffer[BUF_SIZE];
+	LPCWSTR outFile = L"out.txt";
 
 	if (argc != 3) {
-		printf("Использование: cpw test.txt out.txt\n");
+		printf("Использование: cpw test.txt Число\n");
 		return  1;
 	}
+	printf("\n");
 
-	/*LPCWSTR name1 = L"C:"; Для дебага
-	LPCWSTR name2 = L"C:*/
 
 	hIn = CreateFile(argv[1], GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
 	if (hIn == INVALID_HANDLE_VALUE) {
@@ -23,20 +25,45 @@ int wmain(int argc, wchar_t** argv) {
 		return  2;
 	}
 
-	hOut = CreateFile(argv[2], GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	hOut = CreateFile(outFile, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (hOut == INVALID_HANDLE_VALUE) {
 		printf("Невозможно открыть выходной файл. Ошибка: %x\n", GetLastError());
 		return 3;
 	}
 
 	while (ReadFile(hIn, Buffer, BUF_SIZE, &nIn, NULL) && nIn > 0) {
-		INT i = 0, numReplace = 0;
-		while (i < nIn) {
-			if (Buffer[i] >= '0' && Buffer[i] <= '9') {
-				Buffer[i] = ' ';
-				numReplace++;
+		INT numOfNum = 0, index = 0;
+		while (index < nIn) { //Количество цифр во всем буффере
+			if (Buffer[index] >= '0' && Buffer[index] <= '9') {
+				numOfNum++;
 			}
-			i++;
+			index++;
+		}
+
+		INT numFromCMD = _wtoi(argv[2]);
+		if (numOfNum == 0 || numFromCMD <= 0) { //Проверка количества введнных цифр
+			printf("Нельзя заменено 0 цифр(ы), либо в документе они отсутствуют.\n");
+			return 5;
+		}
+		if (numOfNum >= numFromCMD) {
+			printf("Будет заменено %d цифр(ы).\n", numFromCMD);
+		}
+		else {
+			printf("Во всем документе %d цифр(ы), а не %d, поэтому будут заменены все.\n\n", numOfNum, numFromCMD);
+			numFromCMD = numOfNum;
+		}
+
+		index = 0;
+		INT numOfReplace = 0;
+		while (index < nIn) {
+			if (Buffer[index] >= '0' && Buffer[index] <= '9') {
+				Buffer[index] = ' ';
+				numOfReplace++;
+				if (numOfReplace == numFromCMD) {
+					index = nIn;
+				}
+			}
+			index++;
 		}
 
 		WriteFile(hOut, Buffer, nIn, &nOut, NULL);
@@ -44,7 +71,7 @@ int wmain(int argc, wchar_t** argv) {
 			printf("Неустранимая ошибка записи: %x\n", GetLastError());
 			return 4;
 		}
-		printf("Количество замен: %d", numReplace);
+		printf("Количество замененных цифр: %d", numFromCMD);
 	}
 
 	CloseHandle(hIn);
