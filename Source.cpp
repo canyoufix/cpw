d9851054037ae6c11e5def7793194a3bca4074d7
raspberry tea <sekiro.fpl@gmail.com>
2022-11-08 18:03:08 +0700

    v.2.3. Lab3

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <stdio.h> 
 
@@ -5,7 +6,7 @@
 
 INT(*changeBuffer)(CHAR[], DWORD*, INT);
 
-int wmain(int argc, wchar_t** argv) {
+int main(int argc, char* argv[]) {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
@@ -13,17 +14,19 @@ int wmain(int argc, wchar_t** argv) {
 	HINSTANCE hLib;
 
 	DWORD nIn, nOut;
-	CHAR Buffer[BUF_SIZE];
-	LPCWSTR outFile = L"out.txt";
+	CHAR Buffer[BUF_SIZE], outFile[BUF_SIZE];
+	char* cmdLine;
 
-	if (argc != 3) { //Проверка количества аргументов
-		printf("Использование: cpw test.txt Число\n");
-		return  1;
+	if (argc < 3) {
+		printf("No file to process!\n");
+		exit(-1);
 	}
+
+	cmdLine = (char*)GetCommandLine();
 	printf("\n");
 
 	//DLL
-	hLib = LoadLibrary(L"DLL.dll");
+	hLib = LoadLibrary("DLL.dll");
 	if (hLib == NULL) {
 		printf("Не удалось загрузить библиотеку.");
 		exit(-1);
@@ -34,13 +37,19 @@ int wmain(int argc, wchar_t** argv) {
 		exit(-2);
 	}
 
-
-
 	hIn = CreateFile(argv[1], GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
 	if (hIn == INVALID_HANDLE_VALUE) {
 		printf("Невозможно открыть входной файл. Ошибка: %x\n", GetLastError());
 		return  2;
 	}
+
+	int i = 0;
+	while (*(argv[1] + i) != '.') {
+		outFile[i] = *(argv[1] + i);
+		i++;
+	}
+	outFile[i] = '\0';
+	strcat(outFile, "_New.txt");
 
 	hOut = CreateFile(outFile, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (hOut == INVALID_HANDLE_VALUE) {
@@ -48,10 +57,11 @@ int wmain(int argc, wchar_t** argv) {
 		return 3;
 	}
 
+	INT numOfReplace = 0;
 	while (ReadFile(hIn, Buffer, BUF_SIZE, &nIn, NULL) && nIn > 0) {
-		INT numFromCMD = _wtoi(argv[2]);
+		INT numFromCMD = atoi(argv[argc - 1]);
 
-		INT numOfReplace = (*changeBuffer)(Buffer, &nIn, numFromCMD);
+		numOfReplace = (*changeBuffer)(Buffer, &nIn, numFromCMD);
 		if (numOfReplace == -1) {
 			return 5;
 		}
@@ -64,8 +74,10 @@ int wmain(int argc, wchar_t** argv) {
 		printf("Количество замененных цифр: %d", numOfReplace);
 	}
 
+	printf("\n(ProcesslD: %lu), File %s inserts = %d\n", GetCurrentProcessId(), argv[1], numOfReplace);
 	CloseHandle(hIn);
 	CloseHandle(hOut);
 	FreeLibrary(hLib);
-	return 0;
+
+	return numOfReplace;
 }
